835ad042d85efb1e9d71fe481683952997ff9071
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-09-30 08:49:48 -0400

    check for session and fixed typescript error

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
 import SignIn from '@/components/profile/signin'
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
+import { useAuth } from '@/providers/SuperbaseProvider'
 import React from 'react'
 
 function profile() {
+    const { session } = useAuth()
     return (
         <>
-            <SignIn />
+            {!session ? <SignIn /> : <ThemedView><ThemedText>Welcome to your page</ThemedText></ThemedView>}
         </>
     )
 }
--- providers/SuperbaseProvider.tsx
+++ providers/SuperbaseProvider.tsx
@@ -72,7 +72,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
         });
 
         return () => {
-            authListener?.unsubscribe();
+            authListener.subscription.unsubscribe();
         };
     }, []);
 
