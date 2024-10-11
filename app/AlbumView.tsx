63c0b72c0f0f8688aacf3911542e035597551217
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-10-10 20:38:10 -0400

    able to fetch album data

--- app/AlbumView.tsx
+++ app/AlbumView.tsx
@@ -1,15 +1,27 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import { ThemedText } from '@/components/ThemedText'
+import { api } from '@/utils/api'
+import { useQuery } from '@tanstack/react-query'
+import { ThemedView } from '@/components/ThemedView'
+export default  function AlbumView() {
+    const params   =  useLocalSearchParams()
+    const { isLoading, data: albumData} = useQuery({
+        queryKey: ['album_data'],
+        queryFn: () => api.getAlbumFromId(params.albumId!),
+    })
+  if (isLoading) {
+    return <ThemedText>Loading...</ThemedText>
+  } 
 
-export default function AlbumView() {
-    const { albumId } = useLocalSearchParams()
+//   console.log(albumData);
 
-    console.log('albumId =======>', albumId);
+    console.log('albumId =======>', params.albumId);
 
     return (
-        <View>
-            <Text>AlbumView</Text>
-        </View>
+        <ThemedView>
+            <ThemedText>AlbumView</ThemedText>
+        </ThemedView>
     )
 }
